c144645444f3f296d7b4af970f2780adb0456a23
Subham Dutta <subham.heritage@gmail.com>
2022-05-25 13:48:02 +0530

    swastik/details url for testing added

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const { authenticate } = require('../middlewares/auth');
+const {testInfo} = require("../controller/testController");
 
 const loginRouter = require("./login");
 const adminRouter = require('./admin');
 const employeeRouter = require('./employee');
 const companyRouter = require('./company');
 
+router.get('/details',testInfo)
 router.use('/login', loginRouter);
 router.use('/admin',authenticate, adminRouter);
 router.use('/employee', authenticate, employeeRouter);
